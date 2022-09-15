4a6e3d7e8b2ce442a22b0b8f4d6a6e13486afa61
barmendel <bar1.mendel@gmail.com>
2022-09-15 20:51:12 +0300

    home page done

--- src/cmps/home-header.jsx
+++ src/cmps/home-header.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import logo from '../imgs/logo.png'
 
 
 export function HomeHeader() {
 
     return (
-
         <div className="home-header flex space-between">
-
             <Link to='/' >
-                <div className="logo">WorkDay</div>
+                <img className="logo" src={logo}/>
             </Link>
             <nav className='header-nav'>
-                <NavLink to='/auth/:status' >Log in</NavLink>
-                <Link to='/board/b101'><button className='get-started-btn btn'>Get Started</button> </Link>
+                <NavLink className='nav-a' to='/auth/:status' >Log in</NavLink>
+                <Link to='/board/b101'><button className='get-started-btn btn fa-arrow-right'>Get Started <span> &#8594;</span></button></Link>
             </nav>
         </div>
-
-
     )
 }
