60327c4a05d3c5b051351bc8e73c89c0642a9d9d
shaniamos <shaniamos07@gmail.com>
2022-09-19 14:21:14 +0300

    merge changes

--- src/cmps/search.jsx
+++ src/cmps/search.jsx
@@ -32,10 +32,6 @@ export const SearchBoard = ({ onChangeFilter, contentSearch }) => {
                     onChange={handleChange}
                     autoFocus /> */}
             </div>
-
-
         </form>
-
-
     )
 }
