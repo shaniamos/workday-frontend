1221a3b3ccc1eb17049dcd514e2f0a571aa02a3e
shaniamos <shaniamos07@gmail.com>
2022-09-20 02:57:30 +0300

    Labels dropdown modal open-close

--- src/cmps/board/task/status-display.jsx
+++ src/cmps/board/task/status-display.jsx
@@ -3,75 +3,37 @@ import { useState } from "react"
 
 
 export const StatusTypeDisplay = ({ label, value, options }) => {
-    const [isShownClass, setIsShownClass] = useState('')
-    const onLabelClick = () => {
-        setIsShownClass((isShownClass) ? '' : 'show')
+    const [isLabelsOpen, setIsLabelsOpen] = useState('')
+
+    const getColorByValue = (value) => {
+        console.log(options)
+        const option = options.find(option => option.title === value)
+        console.log(option, value)
+        console.log(option.id)
+        return option
     }
-    const getColorByLabel = (label, value) => {
-        switch (label) {
-            case 'status':
-                return getStatusTypeColor(value)
-                break;
-            case 'priority':
-                return getPriorityTypeColor(value)
-            default:
-                break;
-        }
+    const onOpenLabels = () => {
+        setIsLabelsOpen('show')
     }
 
-    const getStatusTypeColor = (value) => {
-        let colorName
-        switch (value) {
-            case 'Done':
-                colorName = '--color-done-green' //'#00c875'
-                return colorName
-            case 'Stuck':
-                colorName = '--color-stuck-red' // '#e2445c'
-                return colorName
-            case 'Working on it':
-                colorName = '--color-orange' // '#fdab3d'
-                return colorName
-            case 'On Hold':
-                colorName = '--color-lipstick' //'#ff5ac4'
-                return colorName
-            default:
-                colorName = '--color-wolf_gray' //c3c6d4
-                return colorName
-        }
+    const onCloseLabels = () => {
+        setIsLabelsOpen('')
     }
-    const getPriorityTypeColor = (value) => {
-        let colorName
-        switch (value) {
-            case 'High':
-                colorName = '--color-orange' //'#fdab3d'
-                return colorName
-            case 'Low':
-                colorName = '--color-done-green' // '#00c875'
-                return colorName
-            case 'Medium':
-                colorName = '--color-bright-blue' // '#579bfc'
-                return colorName
-            case 'Critical':
-                colorName = '--color-error' //'#d83a52'
-                return colorName
-            default:
-                colorName = ' --color-wolf_gray' //c3c6d4
-                return colorName
-        }
-    }
-
     return <React.Fragment>
-
-        <div className="label-dropdown">
-            <div className="cell label-type status-header btn-open-label-drop" onClick={onLabelClick} style={{ backgroundColor: `var(${getColorByLabel(label, value)})` }}>
+        <div className={`label-dropdown ${isLabelsOpen}`}>
+            <div className="main-screen" onClick={onCloseLabels}></div>
+            <div className="cell label-type status-header btn-open-label-drop" onClick={onOpenLabels}
+                // style={{ backgroundColor: `var(${getColorByValue(value)})` }}
+            // style={{ backgroundColor: `var(${getColorByLabel(label, value)})` }}
+            >
                 {`${value}`}
             </div>
             {/* <button onclick="myFunction()" class="dropbtn">Dropdown</button> */}
-            <div className={`dropdown-labels-content ${isShownClass}`}>
+            <div className={`dropdown-labels-content ${isLabelsOpen} `}>
                 <div className="picker-content">
                     <ul className="picker-list" style={{ gridTemplateRows: `repeat(${options.length}, auto)` }}>
                         {options.map(option => {
-                            return <li key={option.id} className="option" style={{backgroundColor: `var(${option.id})`}} onClick={() => { console.log('Option') }}>{option.title}</li>
+                            return <li key={option.id} className="option" style={{ backgroundColor: `var(${option.id})` }} onClick={() => { console.log('Option') }}>{option.title}</li>
                         })}
                     </ul>
                 </div>
