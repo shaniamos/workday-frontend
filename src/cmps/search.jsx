99570a1112b6538291789b6ffb23a3d83cfe3ed9
barmendel <bar1.mendel@gmail.com>
2022-09-24 23:12:29 +0300

    fix search input like monday

--- src/cmps/search.jsx
+++ src/cmps/search.jsx
@@ -1,15 +1,15 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react"
 import { BsSearch } from 'react-icons/bs'  //Search
 
 
-export const Search = ({onChangeFilter, contentSearch, sortOption}) => {
+export const Search = ({ onChangeFilter, contentSearch, sortOption }) => {
     const [txt, setInputTxt] = useState('')
-    
+
     useEffect(() => {
         onChangeFilter({ txt })
     }, [txt])
 
- 
+
     const handleChange = async (ev) => {
         const value = ev.target.value
         setInputTxt(value)
@@ -18,14 +18,15 @@ export const Search = ({onChangeFilter, contentSearch, sortOption}) => {
 
     return (
         <form className="search-board-form">
-             <label htmlFor="txt" ></label>
-             <div className="input-container flex align-center">
-                 <BsSearch />
+            <label htmlFor="txt" ></label>
+            <div className="input-container flex align-center">
+                <div className="bla flex align-center">
+                    <BsSearch />
                     <input
                         className="board-search" autoComplete="off" type="text" name="txt" id="txt"
-                        value={txt}  placeholder= " Search" onChange={handleChange}
-                        autoFocus />            
+                        value={txt} placeholder="Search" onChange={handleChange}/>
                 </div>
+            </div>
         </form>
     )
 }
