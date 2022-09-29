d37113fc573c0707aa542b8cc2e7c068922550dc
Tal <tale1000@gmail.com>
2022-09-29 23:32:48 +0300

    fix user-icon size in main-sidebar cmp

--- src/cmps/home/home-header.jsx
+++ src/cmps/home/home-header.jsx
@@ -8,7 +8,6 @@ import { useSelector } from 'react-redux'
 
 
 export function HomeHeader() {
-    // const loggedinuser = useSelector(state => state.userModule.loggedinuser)
 
     return (
         <React.Fragment>
